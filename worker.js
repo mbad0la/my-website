5af88e3992f4b9a1f0a5e8d90d0d75ea74dc58f4
Mayank Badola <badola21295@gmail.com>
2017-08-19 10:59:18 +0530

    Greedy/Aggresive service worker caching

--- worker.js
+++ worker.js
@@ -1,22 +1,36 @@
-this.addEventListener('install', function (event) {
+self.addEventListener('activate', function(event) {
+  console.log('Activating ...')
+  var cacheWhitelist = ['hack_v5']
+
   event.waitUntil(
-    caches.open('hack_v1')
-      .then(function (cache) {
-        return cache.addAll([
-          '/',
-          '/media/favicon.png',
-          '/media/me.jpg',
-          '/media/projects.json'
-        ])
-      })
+    caches
+    .keys()
+    .then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key)
+        }
+      }))
+    })
   )
 })
 
-this.addEventListener('fetch', function (event) {
+// greedy cache
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        return response || fetch(event.request)
+    caches
+    .match(event.request)
+    .then(function (response) {
+      return response || fetch(event.request).then(function (response) {
+        if (event.request.url.indexOf(self.location.hostname) != -1) {
+          return caches.open('hack_v5').then(function(cache) {
+            cache.put(event.request, response.clone())
+            return response
+          })
+        } else {
+          return response
+        }
       })
+    })
   )
 })
